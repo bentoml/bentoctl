cc64eb79695253b88d8304e44651339f504c5b29
yubozhao <yubz86@gmail.com>
2021-11-10 16:08:31 -0800

    update

--- dev/lint.sh
+++ dev/lint.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
- #
- #GIT_ROOT=$(git rev-parse --show-toplevel)
- #cd "$GIT_ROOT" || exit
- #
- #echo "Running flake8 on bentoml module.."
- #flake8 --config=.flake8 bentoml docker
- #
- #echo "Running flake8 on test module.."
- #flake8 --config=.flake8 tests e2e_tests
- #
- #echo "Running pylint on bentoml module.."
- #pylint --rcfile="./pylintrc" bentoml
- #
- #echo "Running pylint on test module.."
- #pylint --rcfile="./pylintrc" tests e2e_tests
- #
- #echo "Done"
+
+GIT_ROOT=$(git rev-parse --show-toplevel)
+cd "$GIT_ROOT" || exit
+
+echo "Running flake8 on bcdt module.."
+flake8 --config=.flake8 bcdt
+
+echo "Running flake8 on test module.."
+flake8 --config=.flake8 tests
+
+echo "Running pylint on bcdt module.."
+pylint --rcfile="./pylintrc" bcdt
+
+echo "Running pylint on test module.."
+pylint --rcfile="./pylintrc" tests
+
+echo "Done"
