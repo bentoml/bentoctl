b9c2ac3fedfe9d8563d0676cfcb45f851601671e
jjmachan <jamesjithin97@gmail.com>
2021-11-12 12:11:16 +0530

    modifed testoperator to latest spec

---
+++ tests/test-operator/README.md
@@ -0,0 +1,3 @@
+## Test Operator
+
+This is just a test operator that we can use for testing purposes.
---
+++ tests/test-operator/testop/__init__.py
@@ -0,0 +1,7 @@
+from .delete import delete
+from .deploy import deploy
+from .describe import describe
+from .update import update
+
+__all__ = ["deploy", "delete", "describe", "update"]
+
