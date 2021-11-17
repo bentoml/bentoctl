5955678217521c30a1422ff2ca323b5857b7a816
yubozhao <yubz86@gmail.com>
2021-11-16 16:51:59 -0800

    update

--- bcdt/operator/constants.py
+++ bcdt/operator/constants.py
@@ -1,11 +1,13 @@
 OFFICIAL_OPERATORS = {
     "yatai": "bentoml/yatai-deployment-operator",
     "heroku": "bentoml/heroku-deploy",
-    "aws-lambda": "jjmachan/aws-lambda-deploy",
-    "aws-sagemaker": "jjmachan/aws-lambda-deploy",
-    "aws-ec2": "jjmachan/aws-lambda-deploy",
-    "azure-functions": "jjmachan/aws-lambda-deploy",
-    "azure-container": "jjmachan/aws-lambda-deploy",
+    "aws-lambda": "bentoml/aws-lambda-deploy",
+    "aws-sagemaker": "bentoml/aws-sagemaker-deploy",
+    "aws-ec2": "bentoml/aws-ec2-deploy",
+    "azure-functions": "bentoml/azure-functions-deploy",
+    "azure-container-instances": "bentoml/azure-container-instances-deploy",
+    "google-compute-engine": "bentoml/google-compute-engine-deploy",
+    "google-cloud-run": "bentoml/google-cloud-run-deploy",
 }
 
 MAIN_BRANCH = "main"
