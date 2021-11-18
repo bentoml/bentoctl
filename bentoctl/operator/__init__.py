d4501271364ec4492e8cf902f809d07ea2077aa3
jjmachan <jamesjithin97@gmail.com>
2021-11-19 02:54:32 +0530

    expose local registry functions for python api

--- bentoctl/operator/__init__.py
+++ bentoctl/operator/__init__.py
@@ -4,3 +4,10 @@ from bentoctl.operator.utils import _get_bentoctl_home
 
 def get_local_operator_registry():
     return OperatorRegistry(_get_bentoctl_home() / "operators")
+
+
+local_registry = get_local_operator_registry()
+add_from_path = local_registry.add_from_path
+add_official_operator = local_registry.add_official_operator
+add_from_github = local_registry.add_from_github
+add_from_git = local_registry.add_from_git
