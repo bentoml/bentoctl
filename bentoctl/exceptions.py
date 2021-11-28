018c957255670da6f6b927ff4ca704814a81e2e1
Jithin James <jamesjithin97@gmail.com>
2021-11-28 12:31:50 +0530

    fix: small fixes for generate functionalities  (#24)
    * fix: coerce for list field
    
    * feature: keep order in generated file
    
    * fix: cleanup 'help_message' from operator schema
    
    * fix: pretty print validation errors
    
    * fix: input message

--- bentoctl/exceptions.py
+++ bentoctl/exceptions.py
@@ -1,3 +1,6 @@
+import yaml
+
+
 class BentoctlException(Exception):
     """
     Base class for all of bentoctl's exceptions.
@@ -82,10 +85,7 @@ class InvalidDeploymentSpec(BentoctlException):
 
         if msg is None and spec_errors is not None:
             msg_list = ["Error while parsing Deployment Spec."]
-            for field, errors in spec_errors.items():
-                error_msg = "\n".join(errors)
-                msg_list.append(f"{field}: {error_msg}")
-
+            msg_list.append(yaml.safe_dump(spec_errors))
             msg = "\n".join(msg_list)
 
         super(InvalidDeploymentSpec, self).__init__(msg)
