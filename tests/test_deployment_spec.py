8ee8fc305a62a3a3418c22b4ff095779a4020724
jjmachan <jamesjithin97@gmail.com>
2021-11-29 22:48:04 +0530

    test(deployment_spec): more tests

--- tests/test_deployment_spec.py
+++ tests/test_deployment_spec.py
@@ -4,7 +4,7 @@ import pytest
 
 from bentoctl import deployment_spec as dspec
 from bentoctl.operator import OperatorRegistry
-from bentoctl.exceptions import InvalidDeploymentSpec
+from bentoctl.exceptions import InvalidDeploymentSpec, DeploymentSpecNotFound
 from bentoctl.operator.operator import _import_module
 
 from .conftest import TESTOP_PATH
@@ -80,3 +80,65 @@ def test_deployment_spec_init(op_reg, monkeypatch):
     )
     assert dspecobj.bento == TESTOP_PATH
     assert dspecobj.bento_path == Path(TESTOP_PATH)
+
+
+VALID_YAML = """
+api_version: v1
+metadata:
+    name: test
+    operator: testop
+spec:
+    project_id: testproject
+    instances:
+        min: 1
+        max: 2
+"""
+INVALID_YAML = """
+api_version: tst: something: something
+"""
+VALID_YAML_INVALID_SCHEMA = """
+api_version: v1
+metadata:
+    name: test
+    operator: testop
+spec:
+    project_id: testproject
+"""
+
+
+def create_yaml_file(yml_str, path):
+    with open(Path(path, "deployment_spec.yaml"), "w", encoding="utf-8") as f:
+        f.write(yml_str)
+
+
+@pytest.fixture
+def op_reg_with_testop(op_reg, monkeypatch):
+    monkeypatch.setattr(dspec, "local_operator_registry", op_reg)
+    op_reg.add(TESTOP_PATH)
+
+    yield op_reg
+
+
+def test_deployment_spec_from_file(tmp_path, op_reg_with_testop):
+    with pytest.raises(DeploymentSpecNotFound):
+        dspec.DeploymentSpec.from_file(tmp_path / "nofile.yaml")
+
+    create_yaml_file(INVALID_YAML, tmp_path)
+    with pytest.raises(InvalidDeploymentSpec):
+        dspec.DeploymentSpec.from_file(tmp_path / "deployment_spec.yaml")
+
+    create_yaml_file(VALID_YAML, tmp_path)
+    assert dspec.DeploymentSpec.from_file(tmp_path / "deployment_spec.yaml")
+
+
+def test_validate_operator_spec(op_reg_with_testop):
+    operator_config = _import_module("operator_config", TESTOP_PATH)
+    schema = operator_config.OPERATOR_SCHEMA
+
+    import yaml
+    dspec_obj = dspec.DeploymentSpec(yaml.safe_load(VALID_YAML))
+    dspec_obj.validate_operator_spec(schema)
+
+    with pytest.raises(InvalidDeploymentSpec):
+        dspec_obj = dspec.DeploymentSpec(yaml.safe_load(VALID_YAML_INVALID_SCHEMA))
+        dspec_obj.validate_operator_spec(schema)
