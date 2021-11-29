bfafc9c59e33039004e70a464c651a49f6fedbf8
jjmachan <jamesjithin97@gmail.com>
2021-11-29 15:51:52 +0530

    test: install dependencies tests

--- tests/test_operator.py
+++ tests/test_operator.py
@@ -2,10 +2,16 @@
 import os
 import shutil
 import sys
+import types
+from functools import partial
 
 import pytest
 
-from bentoctl.exceptions import OperatorConfigNotFound, OperatorLoadException
+from bentoctl.exceptions import (
+    OperatorConfigNotFound,
+    OperatorLoadException,
+    PipInstallException,
+)
 from bentoctl.operator import operator as operator_module
 
 from .conftest import TESTOP_PATH
@@ -59,3 +65,49 @@ def test_operator_class_operations(tmp_path):
 
     assert operator.describe("third", {}) is not None
     assert operator.delete("forth", {}) is None
+
+
+def test_operator_install_dependencies_with_pip(tmp_path, monkeypatch):
+    testop_path = tmp_path / "test-operator"
+    shutil.copytree(TESTOP_PATH, testop_path)
+    operator = operator_module.Operator(testop_path)
+
+    # no requrirements.txt present
+    assert not operator.install_dependencies()
+
+    # setup patched objects
+    def check_run_command(command, fail_install, **_):
+        assert command == [
+            "pip",
+            "install",
+            "-r",
+            str(testop_path / "requirements.txt"),
+        ]
+        completedprocess = types.SimpleNamespace()
+        completedprocess.stdout = b""
+        completedprocess.stderr = b""
+        if fail_install:
+            completedprocess.returncode = 1
+        else:
+            completedprocess.returncode = 0
+
+        return completedprocess
+
+    # check if success
+    monkeypatch.setattr(
+        operator_module.subprocess,
+        "run",
+        partial(check_run_command, fail_install=False),
+    )
+    (testop_path / "requirements.txt").touch()
+    operator.install_dependencies()
+
+    # check if fail
+    monkeypatch.setattr(
+        operator_module.subprocess,
+        "run",
+        partial(check_run_command, fail_install=True),
+    )
+    (testop_path / "requirements.txt").touch()
+    with pytest.raises(PipInstallException):
+        operator.install_dependencies()
