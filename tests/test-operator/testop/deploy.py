b0f1a0b93214067d3c31728b9fbbb4b8e0cf8665
jjmachan <jamesjithin97@gmail.com>
2021-12-11 15:40:58 +0530

    test: updated testop deployment functions

--- tests/test-operator/testop/deploy.py
+++ tests/test-operator/testop/deploy.py
@@ -2,8 +2,8 @@ import os
 import shutil
 
 
-def deploy(bento_path, deployment_name, lambda_config):
-    print("deploying with: ", bento_path, deployment_name, lambda_config)
+def deploy(bento_path, deployment_name, deployment_spec):
+    print("deploying with: ", bento_path, deployment_name, deployment_spec)
     deployabe_path = os.path.abspath("./testop_deployable")
     cur_path = os.path.dirname(__file__)
     deployable_file = os.path.join(cur_path, "./testdeployable")
--- tests/test-operator/testop/describe.py
+++ tests/test-operator/testop/describe.py
@@ -1,4 +1,4 @@
-def describe(deployment_name, lambda_config):
-    info_json = {"deployment_name": deployment_name, "lambda_config": lambda_config}
+def describe(deployment_name, deployment_spec):
+    info_json = {"deployment_name": deployment_name, "lambda_config": deployment_spec}
 
     return info_json
