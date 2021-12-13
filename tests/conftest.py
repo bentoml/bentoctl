a78c2ee70c5ed68bd3c0249ffff7fef0c7ba936c
jjmachan <jamesjithin97@gmail.com>
2021-12-13 20:10:40 +0530

    test: tests for deployment_config

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,18 @@
-# TODO: remove
+# pylint: disable=W0621
 import os
 
+import pytest
+
+from bentoctl.operator import get_local_operator_registry
+
 TESTOP_PATH = os.path.join(os.path.dirname(__file__), "test-operator")
+
+
+@pytest.fixture
+def op_reg(tmp_path):
+    os.environ["BENTOCTL_HOME"] = str(tmp_path)
+    op_reg = get_local_operator_registry()
+
+    yield op_reg
+
+    del os.environ["BENTOCTL_HOME"]
