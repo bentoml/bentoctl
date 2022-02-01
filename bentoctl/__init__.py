6bfebd5aa323c216932895a8731f785eb1aa04a3
Jithin James <jamesjithin97@gmail.com>
2022-02-02 00:06:04 +0530

    __version__ for bentoctl package (#70)
    * __version__ for bentoctl package
    
    * updated click to 8
    
    * updated rich to lastest

--- bentoctl/__init__.py
+++ bentoctl/__init__.py
@@ -3,6 +3,14 @@ from bentoctl.deployment import deploy_deployment as deploy
 from bentoctl.deployment import describe_deployment as describe
 from bentoctl.deployment import update_deployment as update
 
+try:
+    import importlib.metadata as importlib_metadata
+except ModuleNotFoundError:
+    import importlib_metadata
+
+# Find bentoctl version managed by pyproject.toml
+__version__: str = importlib_metadata.version("bentoctl")
+
 __all__ = [
     "deploy",
     "update",
