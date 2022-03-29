f53b878b629149fa98880a5158bb2a3d25ced198
jjmachan <jamesjithin97@gmail.com>
2022-03-29 22:04:51 +0530

    polish docker utils

--- bentoctl/docker_utils.py
+++ bentoctl/docker_utils.py
@@ -1,7 +1,47 @@
 import os
+from collections import OrderedDict
+
 import docker
+from rich.live import Live
 
 from bentoctl.console import console
+
+
+class DockerPushProgressBar:
+    layers = OrderedDict()
+
+    def sizeof_fmt(self, num, suffix="B"):
+        if num is None:
+            return None
+        for unit in ["", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"]:
+            if abs(num) < 1024.0:
+                return f"{num:3.1f}{unit}{suffix}"
+            num /= 1024.0
+        return f"{num:.1f}Yi{suffix}"
+
+    def format_progress_detail(self, progress_detail):
+        current = self.sizeof_fmt(progress_detail.get("current"))
+        total = self.sizeof_fmt(progress_detail.get("total"))
+
+        if current is None or total is None:
+            return ""
+        else:
+            return f"{current}/{total}"
+
+    def update(self, line):
+        status = line.get("status")
+        layer_id = line.get("id")
+        progress_str = self.format_progress_detail(line.get("progressDetail"))
+        self.layers[layer_id] = {"status": status, "progress_str": progress_str}
+
+    def __rich_console__(self, *_):
+        progress_table = []
+        for layer_id, line in self.layers.items():
+            progress_table.append(
+                f"{layer_id}: {line.get('status')} {line.get('progress_str')}"
+            )
+
+        yield "\n".join(progress_table)
 
 
 def build_docker_image(
@@ -15,6 +55,7 @@ def build_docker_image(
     # make dockerfile relative to context_path
     dockerfile = os.path.relpath(dockerfile, context_path)
     try:
+        # TODO: proper error handling
         output_stream = docker_client.images.client.api.build(
             path=context_path,
             tag=image_tag,
@@ -23,7 +64,7 @@ def build_docker_image(
             decode=True,
         )
         for line in output_stream:
-            print(line.get('stream', ''), end='')    
+            print(line.get("stream", ""), end="")
         console.print(":hammer: Image build!")
     except (docker.errors.APIError, docker.errors.BuildError) as error:
         raise Exception(f"Failed to build docker image {image_tag}: {error}")
@@ -37,7 +78,17 @@ def push_docker_image_to_repository(
     if username is not None and password is not None:
         docker_push_kwags["auth_config"] = {"username": username, "password": password}
     try:
-        out = docker_client.images.push(**docker_push_kwags)
+        # TODO: proper error handling
+        progress_bar = DockerPushProgressBar()
+        with Live(progress_bar) as live:
+            for line in docker_client.images.push(
+                **docker_push_kwags, decode=True, stream=True
+            ):
+                if "id" in line:
+                    progress_bar.update(line)
+                    live.update(progress_bar)
+                elif "status" in line:
+                    print(line.get("status"))
         console.print(":rocket: Image pushed!")
     except docker.errors.APIError as error:
         raise Exception(f"Failed to push docker image {image_tag}: {error}")
