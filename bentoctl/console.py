1201d197ee4efa08667617be60869e2515ffa8fb
Jithin James <jamesjithin97@gmail.com>
2022-04-13 18:44:05 +0530

    fix: improve docs strings and prompts (#111)
    * fix: print only the generated files
    
    * feat(init): improved user prompt for filename
    
    * improve doc strings for operator methods

--- bentoctl/console.py
+++ bentoctl/console.py
@@ -8,7 +8,7 @@ console = Console(highlight=False)
 def print_generated_files_list(generated_files: list):
     console.print(":sparkles: generated template files.")
     for file in generated_files:
-        console.print(f"  - {os.path.join(os.curdir, file)}")
+        console.print(f"  - {file}")
 
 
 def prompt_user_for_filename():
@@ -17,5 +17,11 @@ def prompt_user_for_filename():
     )
     if deployment_config_filname == "":
         deployment_config_filname = "deployment_config.yaml"
+
+    if not (
+        deployment_config_filname.endswith(".yaml")
+        or deployment_config_filname.endswith(".yml")
+    ):
+        deployment_config_filname += ".yaml"
 
     return deployment_config_filname
