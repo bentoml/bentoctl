ddb3d568535d4a575a9afbba0b521a4349840879
yubozhao <yubz86@gmail.com>
2022-04-26 18:47:40 -0700

    update

--- bentoctl/cli/utils.py
+++ bentoctl/cli/utils.py
@@ -5,6 +5,7 @@ import click
 
 from bentoctl.exceptions import BentoctlException
 from bentoctl.utils import set_debug_mode
+from bentoctll.utils.usage_stats import BENTOCTL_DO_NOT_TRACK, cli_events_map
 
 DEBUG_ENV_VAR = "BENTOCTL_DEBUG"
 
@@ -22,7 +23,7 @@ def handle_bentoctl_exceptions(func):
 
 
 class BentoctlCommandGroup(click.Group):
-    NUMBER_OF_COMMON_PARAMS = 1
+    NUMBER_OF_COMMON_PARAMS = 2
 
     @staticmethod
     def bentoctl_common_params(func):
@@ -33,6 +34,13 @@ class BentoctlCommandGroup(click.Group):
             default=False,
             help="Show debug logs when running the command",
         )
+        @click.option(
+            "--do-not-track",
+            is_flag=True,
+            default=False,
+            envvar=BENTOCTL_DO_NOT_TRACK,
+            help="Do not send uage info",
+        )
         @functools.wraps(func)
         def wrapper(verbose, *args, **kwargs):
             set_debug_mode(verbose)
@@ -41,10 +49,50 @@ class BentoctlCommandGroup(click.Group):
 
         return wrapper
 
+    @staticmethod
+    def bentoctl_track_usage(func, cmd_group, **kwargs):
+        command_name = kwargs.get("name", func.__name__)
+
+        @functools.wraps(func)
+        def wrapper(do_not_track: bool, *args, **kwargs):
+            if do_not_track:
+                os.environs["BENTOCTL_DO_NOT_TRACK"] = str(True)
+                return func(*args, **kwargs)
+            start_time = time.time_ns()
+            if (cmd_group.name in cli_events_map and command_name in cli_events_map[cmd_group.name]):
+                get_tracking_event = functools.partial(
+                    cli_events_map[cmd_group.name][command_name],
+                    cmd_group.name,
+                    command_name
+                )
+            else:
+                def get_tracking_event(ret):
+                    return {
+                        cmd_group=cmd_group.name,
+                        cmd_name=command_name,
+                    }
+            try:
+                return_value = func(*args, **kwargs)
+                event = get_tracking_event(return_value)
+                duration_in_ms = time.time_ns() - start_time
+                event.duration_in_ms = duration_in_ms / 1e6
+                track(event)
+                return return_value
+            except Exception as e:
+                event = get_tracking_event(None)
+                duration_in_ms = time.time_ns() - start_time
+                event.duration_in_ms = duration_in_ms / 1e6
+                event.error_type = type(e).__name__
+                event.return_code = 2 if isinstance(e, KeyboardInterrupt) else 1
+                track(event)
+                raise
+        return wrapper
+
     def command(self, *args, **kwargs):
         def wrapper(func):
             # add common parameters to command
             func = BentoctlCommandGroup.bentoctl_common_params(func)
+            func = BentoctlCommandGroup.bentoctl_track_usage(func, self, **kwargs)
 
             # move common parameters to end of the parameters list
             func.__click_params__ = (
--- bentoctl/utils/usage_stats.py
+++ bentoctl/utils/usage_stats.py
@@ -1,5 +1,10 @@
 import os
+import uuid
 import requests
+import yaml
+
+from bentoml._internal.utils.analytics.schemas import CommonProperties
+
 
 BENTOCTL_DO_NOT_TRACK = "BENTOML_DO_NOT_TRACK"
 USAGE_TRACKING_URL = "https://t.bentoml.com"
@@ -13,7 +18,13 @@ def do_not_track() -> bool:
 
 
 def get_payload(event_properties: dict) -> dict:
-    return {}
+    common_properties = CommonProperties()
+    return {
+        "common_properties": common_properties,
+        "event_properties": event_properties,
+        "session_id": uuid.uuid1().hex,
+        "event_type": "bentoctl-cli"
+    }
 
 
 def track(event_properties: dict):
@@ -24,3 +35,31 @@ def track(event_properties: dict):
     requests.post(
         USAGE_TRACKING_URL, json=payload, timeout=USAGE_REQUEST_TIMEOUT_SECONDS
     )
+
+
+def _cli_bentoctl_build_event(cmd_group, cmd_name, cmd_args, cmd_kwargs):
+    return {}
+
+
+cli_events_map = {"cli": {"build": _cli_bentoctl_build_event}}
+# {
+#     'session_id': 'e0511c1ac5a711ecbc9f1c5c60561cbc',
+#     'event_properties': {
+#         'cmd_name': 'serve',
+#         'error_type': None,
+#         'return_code': None
+#     },
+#     'common_properties': {
+#         'timestamp': '2022-04-26T21:28:59.408329+00:00',
+#         'platform': 'Darwin-21.5.0-x86_64-i386-64bit',
+#         'bentoml_version': '1.0.0a7.post9+gea0040f3',
+#         'python_version': '3.7.10',
+#          'is_interactive': False, 'in_notebook': False, 'memory_usage_percent': 0.6437778472900391, 'total_memory_in_mb': 16384,
+#          'client': {
+#              'id': '0cf269e4-ecf7-4869-ba7f-f32bc3d39ee5',
+#              'creation_timestamp': '2022-04-06T22:14:53.694582+00:00'
+#         },
+#         'yatai_user_email': 'admin@abc.com'
+#     },
+#     'event_type': 'cli'
+# }
