62583a10ee9f48ed72fe8381467d1bf699392597
jjmachan <jamesjithin97@gmail.com>
2022-05-23 12:41:11 +0530

    added utils

--- bentoctl/utils/operator_helpers/__init__.py
+++ bentoctl/utils/operator_helpers/__init__.py
@@ -2,5 +2,6 @@ from bentoctl.utils.operator_helpers.create_deployable import (
     create_deployable_from_local_bentostore,
 )
 from bentoctl.utils.operator_helpers.generate import Generate
+from bentoctl.utils.operator_helpers.utils import run_shell_command
 
-__all__ = ["Generate", "create_deployable_from_local_bentostore"]
+__all__ = ["Generate", "create_deployable_from_local_bentostore", "run_shell_command"]
