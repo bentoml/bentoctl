e1b572a5c7b83ada875a550774ae017806acde62
Frost Ming <mianghong@gmail.com>
2023-07-22 13:44:32 +0800

    fix: copy models to bento directory (#210)
    * chore: switch to PDM
    
    Signed-off-by: Frost Ming <me@frostming.com>
    
    * automated release workflow
    
    Signed-off-by: Frost Ming <me@frostming.com>
    
    * ignore certain patterns
    
    Signed-off-by: Frost Ming <me@frostming.com>
    
    * fix lint errors
    
    Signed-off-by: Frost Ming <me@frostming.com>
    
    * fix: copy models to bento dir
    
    Signed-off-by: Frost Ming <me@frostming.com>
    
    ---------
    
    Signed-off-by: Frost Ming <me@frostming.com>
    Co-authored-by: Aaron Pham <29749331+aarnphm@users.noreply.github.com>

--- bentoctl/cli/__init__.py
+++ bentoctl/cli/__init__.py
@@ -31,13 +31,13 @@ from bentoctl.utils.terraform import (
 
 logger = logging.getLogger(__name__)
 try:
-    from bentoml_cli.utils import validate_docker_tag
+    from bentoml_cli.utils import validate_container_tag
 except ImportError:
     logger.warning(
         "'bentoml._internal.utils.docker.validate_tag' not imported. "
         "Validation dissabled."
     )
-    validate_docker_tag = None
+    validate_container_tag = None
 
 
 CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
@@ -143,7 +143,7 @@ def generate(deployment_config_file, values_only, save_path):
     "--docker-image-tag",
     help="Name and optionally a tag (format: 'name:tag'), defaults to bento tag.",
     required=False,
-    callback=validate_docker_tag,
+    callback=validate_container_tag,
     multiple=True,
 )
 @click.option(
